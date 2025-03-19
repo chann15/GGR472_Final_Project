e3de9769a121d3d600ee128be0f4be6b45f2aa16
emilyamoffat <emilyamoffat@gmail.com>
2025-03-19 19:57:33 -0400

    edit whats in map.on
    try to make the updates the prof requested

--- script.js
+++ script.js
@@ -199,12 +199,7 @@ document.getElementById("generate_listings").addEventListener("click", function 
         )
       : true; // If unchecked, ignore this buffer
 
-    // Debugging: Log the results for each point
-    console.log('Point:', point);
-    console.log('In Isochrone:', inIsochrone);
-    console.log('In Grocery Buffer:', inGroceryBuffer);
-    console.log('In Parks Buffer:', inParksBuffer);
-    console.log('In TTC Buffer:', inTTCBuffer);
+
 
     // Add point if it is inside all active buffers
     if (inIsochrone && inGroceryBuffer && inParksBuffer && inTTCBuffer) {
@@ -289,18 +284,49 @@ map.on('load', () => {
 
 });
 
+// move fetch out, and legend code out of java to html, add checkboxes etc to the html - this is being grumpy
+let groceryResponse;
+let parksResponse;
+let ttcResponse;
+
+fetch('https://raw.githubusercontent.com/emilyamoffat/test/main/overpass_grocery.geojson')
+  .then(response => response.json())
+  .then(response => {
+    console.log(response); //Check response in console
+    groceryResponse = response; // Store geojson as variable using URL from fetch response
+
+  });
+
+fetch('https://raw.githubusercontent.com/chann15/GGR472_Final_Project/ab671d350e44e397a5663ec1fb1cdf4d700a5fa9/Data/Parks%20and%20Recreation_TOR.geojson')
+  .then(response => response.json())
+  .then(response => {
+    console.log(response); //Check response in console
+    parksResponse = response; // Store geojson as variable using URL from fetch response
+
+  });
+
+fetch('https://raw.githubusercontent.com/chann15/GGR472_Final_Project/main/Data/TTC%20POINTS.geojson')
+  .then(response => response.json())
+  .then(response => {
+    console.log(response); //Check response in console
+    ttcResponse = response; // Store geojson as variable using URL from fetch response
+
+  });
+
+
 map.on('load', async () => {
-  // Load grocery data
-  const groceryResponse = await fetch('https://raw.githubusercontent.com/emilyamoffat/test/main/overpass_grocery.geojson');
-  const groceryData = await groceryResponse.json();
 
-  // Load Parks and Recreation data
-  const parksResponse = await fetch('https://raw.githubusercontent.com/chann15/GGR472_Final_Project/ab671d350e44e397a5663ec1fb1cdf4d700a5fa9/Data/Parks%20and%20Recreation_TOR.geojson');
-  const parksData = await parksResponse.json();
+  // // Load grocery data
+  // const groceryResponse = await fetch('https://raw.githubusercontent.com/emilyamoffat/test/main/overpass_grocery.geojson');
+  // const groceryData = await groceryResponse.json();
 
-  // Load TTC points data
-  const ttcResponse = await fetch('https://raw.githubusercontent.com/chann15/GGR472_Final_Project/main/Data/TTC%20POINTS.geojson');
-  const ttcData = await ttcResponse.json();
+  // // Load Parks and Recreation data
+  // const parksResponse = await fetch('https://raw.githubusercontent.com/chann15/GGR472_Final_Project/ab671d350e44e397a5663ec1fb1cdf4d700a5fa9/Data/Parks%20and%20Recreation_TOR.geojson');
+  // const parksData = await parksResponse.json();
+
+  // // Load TTC points data
+  // const ttcResponse = await fetch('https://raw.githubusercontent.com/chann15/GGR472_Final_Project/main/Data/TTC%20POINTS.geojson');
+  // const ttcData = await ttcResponse.json();
 
   // Add sources for buffers
   map.addSource('grocery-buffered-data', { type: 'geojson', data: { type: 'FeatureCollection', features: [] } });
